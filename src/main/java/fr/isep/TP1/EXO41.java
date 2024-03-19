a7b44e32bd237981ce920e5c186d79b617b4d214
Ficelo <teiva.trillard@gmail.com>
2024-03-19 11:55:24 +0100

    tet lien avec intelij

--- src/main/java/fr/isep/TP1/EXO41.java
+++ src/main/java/fr/isep/TP1/EXO41.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 public class EXO41 {
 
     public static void main(String[] args) {
+
+        // GIGA EXO DE MALADE MENTAL (test pour git)
+
         System.out.println("Bonjour, quel est votre prénom ?");
         Scanner scanner = new Scanner(System.in);
 
